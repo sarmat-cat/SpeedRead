000739869aed80612d1c601b9b8d80e2eb93e3eb
sarmat-cat <ulyanova.nastiya@gmail.com>
2020-05-26 03:14:52 +0400

    Прчесали табсы, добавили задания

--- Speed reading/Speed reading/Form1.Designer.cs
+++ Speed reading/Speed reading/Form1.Designer.cs
@@ -30,8 +30,7 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.textP2 = new System.Windows.Forms.TabPage();
-            this.text1text = new System.Windows.Forms.RichTextBox();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.text1start = new System.Windows.Forms.Button();
             this.text1finish = new System.Windows.Forms.Button();
@@ -42,70 +41,67 @@
             this.text1P1 = new System.Windows.Forms.TabPage();
             this.text1next = new System.Windows.Forms.Button();
             this.infoText1 = new System.Windows.Forms.RichTextBox();
-            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.webBrowser2 = new System.Windows.Forms.WebBrowser();
             this.tabControl1.SuspendLayout();
-            this.textP2.SuspendLayout();
-            this.tabPage2.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             this.tabControl2.SuspendLayout();
             this.menuP1.SuspendLayout();
             this.text1P1.SuspendLayout();
+            this.tabPage4.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
             // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.tabControl1.Appearance = System.Windows.Forms.TabAppearance.FlatButtons;
-            this.tabControl1.Controls.Add(this.textP2);
+            this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.ItemSize = new System.Drawing.Size(60, 20);
-            this.tabControl1.Location = new System.Drawing.Point(208, 12);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.ItemSize = new System.Drawing.Size(70, 20);
+            this.tabControl1.Location = new System.Drawing.Point(156, 10);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(580, 659);
+            this.tabControl1.Size = new System.Drawing.Size(435, 535);
             this.tabControl1.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
             this.tabControl1.TabIndex = 0;
             this.tabControl1.TabStop = false;
             // 
-            // textP2
+            // tabPage1
             // 
-            this.textP2.Controls.Add(this.text1text);
-            this.textP2.Location = new System.Drawing.Point(4, 24);
-            this.textP2.Name = "textP2";
-            this.textP2.Padding = new System.Windows.Forms.Padding(3);
-            this.textP2.Size = new System.Drawing.Size(572, 631);
-            this.textP2.TabIndex = 0;
-            this.textP2.Text = "textP2";
-            this.textP2.UseVisualStyleBackColor = true;
-            // 
-            // text1text
-            // 
-            this.text1text.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.text1text.BackColor = System.Drawing.SystemColors.Window;
-            this.text1text.Location = new System.Drawing.Point(8, 8);
-            this.text1text.Name = "text1text";
-            this.text1text.ReadOnly = true;
-            this.text1text.Size = new System.Drawing.Size(700, 743);
-            this.text1text.TabIndex = 1;
-            this.text1text.Text = "";
-            this.text1text.ZoomFactor = 1.4F;
+            this.tabPage1.Controls.Add(this.webBrowser1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage1.Size = new System.Drawing.Size(427, 507);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Разминка";
+            this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // tabPage2
             // 
-            this.tabPage2.Controls.Add(this.webBrowser);
             this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(572, 631);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage2.Size = new System.Drawing.Size(427, 507);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.Text = "Тренировка";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // text1start
             // 
-            this.text1start.Location = new System.Drawing.Point(31, 242);
+            this.text1start.Location = new System.Drawing.Point(23, 197);
+            this.text1start.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.text1start.Name = "text1start";
-            this.text1start.Size = new System.Drawing.Size(134, 58);
+            this.text1start.Size = new System.Drawing.Size(100, 47);
             this.text1start.TabIndex = 0;
             this.text1start.Text = "Начать!";
             this.text1start.UseVisualStyleBackColor = true;
@@ -113,9 +109,10 @@
             // 
             // text1finish
             // 
-            this.text1finish.Location = new System.Drawing.Point(6, 528);
+            this.text1finish.Location = new System.Drawing.Point(4, 429);
+            this.text1finish.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.text1finish.Name = "text1finish";
-            this.text1finish.Size = new System.Drawing.Size(180, 72);
+            this.text1finish.Size = new System.Drawing.Size(135, 58);
             this.text1finish.TabIndex = 2;
             this.text1finish.Text = "Готово!";
             this.text1finish.UseVisualStyleBackColor = true;
@@ -126,10 +123,11 @@
             this.tabControl2.Appearance = System.Windows.Forms.TabAppearance.FlatButtons;
             this.tabControl2.Controls.Add(this.menuP1);
             this.tabControl2.Controls.Add(this.text1P1);
-            this.tabControl2.Location = new System.Drawing.Point(6, 8);
+            this.tabControl2.Location = new System.Drawing.Point(4, 6);
+            this.tabControl2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabControl2.Name = "tabControl2";
             this.tabControl2.SelectedIndex = 0;
-            this.tabControl2.Size = new System.Drawing.Size(200, 663);
+            this.tabControl2.Size = new System.Drawing.Size(150, 539);
             this.tabControl2.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
             this.tabControl2.TabIndex = 1;
             this.tabControl2.TabStop = false;
@@ -138,19 +136,21 @@
             // 
             this.menuP1.Controls.Add(this.StartTest);
             this.menuP1.Controls.Add(this.infoMenu);
-            this.menuP1.Location = new System.Drawing.Point(4, 28);
+            this.menuP1.Location = new System.Drawing.Point(4, 25);
+            this.menuP1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.menuP1.Name = "menuP1";
-            this.menuP1.Padding = new System.Windows.Forms.Padding(3);
-            this.menuP1.Size = new System.Drawing.Size(192, 631);
+            this.menuP1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.menuP1.Size = new System.Drawing.Size(142, 510);
             this.menuP1.TabIndex = 0;
             this.menuP1.Text = "menuP1";
             this.menuP1.UseVisualStyleBackColor = true;
             // 
             // StartTest
             // 
-            this.StartTest.Location = new System.Drawing.Point(7, 206);
+            this.StartTest.Location = new System.Drawing.Point(5, 167);
+            this.StartTest.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.StartTest.Name = "StartTest";
-            this.StartTest.Size = new System.Drawing.Size(179, 52);
+            this.StartTest.Size = new System.Drawing.Size(134, 42);
             this.StartTest.TabIndex = 1;
             this.StartTest.Text = "Начать тренировку";
             this.StartTest.UseVisualStyleBackColor = true;
@@ -158,10 +158,11 @@
             // 
             // infoMenu
             // 
-            this.infoMenu.Location = new System.Drawing.Point(7, 7);
+            this.infoMenu.Location = new System.Drawing.Point(5, 6);
+            this.infoMenu.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.infoMenu.Name = "infoMenu";
             this.infoMenu.ReadOnly = true;
-            this.infoMenu.Size = new System.Drawing.Size(179, 178);
+            this.infoMenu.Size = new System.Drawing.Size(135, 145);
             this.infoMenu.TabIndex = 0;
             this.infoMenu.Text = "";
             // 
@@ -171,19 +172,21 @@
             this.text1P1.Controls.Add(this.infoText1);
             this.text1P1.Controls.Add(this.text1finish);
             this.text1P1.Controls.Add(this.text1start);
-            this.text1P1.Location = new System.Drawing.Point(4, 28);
+            this.text1P1.Location = new System.Drawing.Point(4, 25);
+            this.text1P1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.text1P1.Name = "text1P1";
-            this.text1P1.Padding = new System.Windows.Forms.Padding(3);
-            this.text1P1.Size = new System.Drawing.Size(192, 631);
+            this.text1P1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.text1P1.Size = new System.Drawing.Size(142, 510);
             this.text1P1.TabIndex = 1;
             this.text1P1.Text = "text1P1";
             this.text1P1.UseVisualStyleBackColor = true;
             // 
             // text1next
             // 
-            this.text1next.Location = new System.Drawing.Point(31, 306);
+            this.text1next.Location = new System.Drawing.Point(23, 249);
+            this.text1next.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.text1next.Name = "text1next";
-            this.text1next.Size = new System.Drawing.Size(134, 58);
+            this.text1next.Size = new System.Drawing.Size(100, 47);
             this.text1next.TabIndex = 4;
             this.text1next.Text = "Следующее задание";
             this.text1next.UseVisualStyleBackColor = true;
@@ -191,38 +194,68 @@
             // 
             // infoText1
             // 
-            this.infoText1.Location = new System.Drawing.Point(4, 7);
+            this.infoText1.Location = new System.Drawing.Point(3, 6);
+            this.infoText1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.infoText1.Name = "infoText1";
-            this.infoText1.Size = new System.Drawing.Size(182, 220);
+            this.infoText1.Size = new System.Drawing.Size(138, 180);
             this.infoText1.TabIndex = 3;
             this.infoText1.Text = "";
             // 
-            // webBrowser
+            // tabPage3
             // 
-            this.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.webBrowser.Location = new System.Drawing.Point(3, 3);
-            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
-            this.webBrowser.Name = "webBrowser";
-            this.webBrowser.Size = new System.Drawing.Size(566, 625);
-            this.webBrowser.TabIndex = 0;
+            this.tabPage3.Location = new System.Drawing.Point(4, 24);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(427, 507);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Тест";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.webBrowser2);
+            this.tabPage4.Location = new System.Drawing.Point(4, 24);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(427, 507);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Доп. материалы";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(2, 2);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(423, 503);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // webBrowser2
+            // 
+            this.webBrowser2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser2.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser2.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser2.Name = "webBrowser2";
+            this.webBrowser2.Size = new System.Drawing.Size(427, 507);
+            this.webBrowser2.TabIndex = 0;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 683);
+            this.ClientSize = new System.Drawing.Size(600, 555);
             this.Controls.Add(this.tabControl2);
             this.Controls.Add(this.tabControl1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Speed reading";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.tabControl1.ResumeLayout(false);
-            this.textP2.ResumeLayout(false);
-            this.tabPage2.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
             this.tabControl2.ResumeLayout(false);
             this.menuP1.ResumeLayout(false);
             this.text1P1.ResumeLayout(false);
+            this.tabPage4.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -230,10 +263,9 @@
         #endregion
 
         private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage textP2;
+        private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.Button text1start;
-        private System.Windows.Forms.RichTextBox text1text;
         private System.Windows.Forms.Button text1finish;
         private System.Windows.Forms.TabControl tabControl2;
         private System.Windows.Forms.TabPage menuP1;
@@ -242,7 +274,10 @@
         private System.Windows.Forms.RichTextBox infoMenu;
         private System.Windows.Forms.RichTextBox infoText1;
         private System.Windows.Forms.Button text1next;
-        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.WebBrowser webBrowser2;
     }
 }
 
--- Speed reading/Speed reading/Form1.cs
+++ Speed reading/Speed reading/Form1.cs
@@ -15,6 +15,7 @@ namespace Speed_reading
     public partial class Form1 : Form
     {
         int window = 0;
+        int read_1_cnt = 1;
 
         string text = "текст тестовый, обычный такой";
 
@@ -27,6 +28,7 @@ namespace Speed_reading
         private void Form1_Load(object sender, EventArgs e)
         {
             text1finish.Hide();
+            webBrowser2.DocumentText = File.ReadAllText(@"tabl.html");
         }
 
         private int dispetchr (int window)
@@ -37,16 +39,16 @@ namespace Speed_reading
 
         private int Menu()
         {
-            tabControl1.SelectedTab = textP2;
-            text1text.Text = "";
+            tabControl1.SelectedTab = tabPage1;
+           // text1text.Text = "";
             tabControl2.SelectedTab = menuP1;
             return 0;
         }
 
         private int TestText1()
         {
-            tabControl1.SelectedTab = textP2;
-            text1text.Text = "";
+            tabControl1.SelectedTab = tabPage1;
+            //text1text.Text = "";
             tabControl2.SelectedTab = text1P1;
             infoText1.Text = "описание задания и того что нужно сделать";
             text1finish.Hide();
@@ -54,16 +56,39 @@ namespace Speed_reading
             text1next.Hide();
             return 0;
         }
-
+        // Подумать!!!
+        // В зависисмости от открытой вкладки (TabPage) 
+        // применять разные действия по кликам 
+        // через конструкцию switch case...
+        // тогда кнопки управления будут общими
         private void Text1Start_Click(object sender, EventArgs e)
         {
-            Random rnd = new Random();
-            text1text.Text = text;
-            text1text.Text = File.ReadAllText(@"text_"+ rnd.Next(2,4).ToString() +".txt");
-            webBrowser.DocumentText = File.ReadAllText(@"read.html");
+            switch(tabControl1.SelectedTab.Name)
+                {
+                case "tabPage1":
+                    Random rnd = new Random();
+                    //text1text.Text = text;
+                    //text1text.Text = File.ReadAllText(@"text_"+ rnd.Next(2,4).ToString() +".txt");
+                    webBrowser1.DocumentText = File.ReadAllText(@"read_1_" + read_1_cnt.ToString() + "_" + rnd.Next(1, 3).ToString() + ".html");
+                    // webBrowser.DocumentText = File.ReadAllText(@"read.html");
+                    read_1_cnt = read_1_cnt >= 2 ? 1 : read_1_cnt + 1;     
+            
+                break;
+
+                default:
+
+                break;
+            }
+
+
+   
+                
+            
             this.TimeText.Start();
             text1start.Hide();
             text1finish.Show();
+            
+  
 
         }
 
@@ -74,6 +99,7 @@ namespace Speed_reading
             text1finish.Hide();
             text1next.Show();
             int countWord = 1;
+            text = webBrowser1.Document.Body.InnerText;
             for (int i = 0; i < text.Length; i++)
             {
                 if (text[i] == ' ')
